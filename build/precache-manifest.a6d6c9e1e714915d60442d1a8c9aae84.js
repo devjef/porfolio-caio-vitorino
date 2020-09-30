8e2de1d73060e2364dfad283ab4899e7ffa0387d
Jeferson <36243087+devjef@users.noreply.github.com>
2020-09-30 17:59:19 -0300

    Add files via upload

---
+++ build/precache-manifest.a6d6c9e1e714915d60442d1a8c9aae84.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8171ed12fcc40a909312d38f0369428b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c064e0bab564afd8879b",
+    "url": "/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "5edf6b32279f79796bd0",
+    "url": "/static/css/main.48abe73d.chunk.css"
+  },
+  {
+    "revision": "c064e0bab564afd8879b",
+    "url": "/static/js/2.f11f410b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f11f410b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5edf6b32279f79796bd0",
+    "url": "/static/js/main.bcf40ad5.chunk.js"
+  },
+  {
+    "revision": "ed1132c713b473bd4389",
+    "url": "/static/js/runtime-main.a7a6da1b.js"
+  },
+  {
+    "revision": "fd8469cf8487d6acc461d0a243dc14e1",
+    "url": "/static/media/Logo-caio-2.fd8469cf.svg"
+  },
+  {
+    "revision": "81d3f23ab7cdbd66796f9e5935d48635",
+    "url": "/static/media/Logo-caio-amarelo-2.81d3f23a.svg"
+  },
+  {
+    "revision": "3d45f423d62a5020c05c928ad8772391",
+    "url": "/static/media/arrow.3d45f423.svg"
+  },
+  {
+    "revision": "a74212b632dc205e2bd832acaa46a2e9",
+    "url": "/static/media/arrow.a74212b6.svg"
+  },
+  {
+    "revision": "5a2654d838c264f510cfde8aabed2320",
+    "url": "/static/media/call.5a2654d8.jpg"
+  },
+  {
+    "revision": "04e4f259c38bea403acb2148d9f90aa0",
+    "url": "/static/media/contact-info-icon1.04e4f259.svg"
+  },
+  {
+    "revision": "df4fdedf2f261b6bd88551a4f1ab92f9",
+    "url": "/static/media/contact-info-icon2.df4fdedf.svg"
+  },
+  {
+    "revision": "3cf93bcfa4d4d754bc4675b37bc0361b",
+    "url": "/static/media/contact-info-icon3.3cf93bcf.svg"
+  },
+  {
+    "revision": "419dac0e4856dfb26858a0072d516b94",
+    "url": "/static/media/facebook.419dac0e.svg"
+  },
+  {
+    "revision": "58992cae52e68d52719dffe82b190bbb",
+    "url": "/static/media/instagram.58992cae.svg"
+  },
+  {
+    "revision": "789bce482c497ca5006b97bfe6f0f91a",
+    "url": "/static/media/logo-yellow.789bce48.svg"
+  },
+  {
+    "revision": "f521cf4553d6d240c47e135be9ba7a9c",
+    "url": "/static/media/mobile-close.f521cf45.svg"
+  },
+  {
+    "revision": "10823520de33dffb05e6963a0d8c7bec",
+    "url": "/static/media/mobile-menu.10823520.svg"
+  },
+  {
+    "revision": "762a2f17fb7647d35fc0eb237a2be4d1",
+    "url": "/static/media/perfil.762a2f17.jpg"
+  },
+  {
+    "revision": "2d9a52846b82698c874cc4e9460d9180",
+    "url": "/static/media/youtube.2d9a5284.svg"
+  }
+]);
